db51f69317a47e1e8b68c35e288c3c9dcd836ad3
Krister Ross <k@mcminn.no>
2023-11-28 13:13:52 +0100

    added

--- resources/views/keywords.blade.php
+++ resources/views/keywords.blade.php
@@ -1,9 +1,26 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Keywords') }}
-        </h2>
-    </x-slot>
+     <!-- Menu -->
+     <nav class="bg-white leading-tight">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center justify-between h-16">
+                <div class="flex items-center">
+                    <div class="flex-shrink-0">
+                        <!-- Logo or Brand Name Here -->
+                    </div>
+                    <div class="hidden md:block">
+                        <div class="ml-10 flex items-baseline space-x-4">
+                            <!-- Navigation Links -->
+                            <a href="#" class="text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                            <a href="#" class="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Features</a>
+                            <a href="#" class="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Pricing</a>
+                            <!-- Add more links as needed -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
